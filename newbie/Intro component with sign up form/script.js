b4e187d50d434734aae89f557634997e2f4a4263
drmazenaskar <248049118+drmazenaskar-blip@users.noreply.github.com>
2026-09-18 15:24:57 +0300

    Add intro component sign up form

---
+++ newbie/Intro component with sign up form/script.js
@@ -0,0 +1,51 @@
+const form = document.querySelector(".singup-form");
+const fields = document.querySelectorAll(".input-group  input");
+const password = document.querySelector("#password");
+const eyeIcon = document.querySelector(".fa-eye");
+// console.log(password)
+
+function checkValiedInput(input) {
+  let inputParent = input.parentElement;
+
+  if (input.value.trim() === "") {
+    inputParent.classList.add("error");
+    return false;
+  } else {
+    inputParent.classList.remove("error");
+    return true;
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let checkStatus = true;
+
+  fields.forEach(function (input) {
+    let status = checkValiedInput(input);
+
+    if (!status) {
+      checkStatus = false;
+    }
+  });
+
+  if (checkStatus) {
+    form.reset();
+  }
+});
+fields.forEach(function (input) {
+  input.addEventListener("input", () => {
+    checkValiedInput(input);
+  });
+});
+// -----Password Input-------------------------------------
+function togglePassword() {
+  password.type = password.type === "password" ? "text" : "password";
+}
+function showPasswordIcon() {
+  eyeIcon.classList.toggle("display", password.value.trim() !== "");
+}
+password.oninput = showPasswordIcon;
+eyeIcon.onclick = togglePassword;
+
+// ===========================================================
